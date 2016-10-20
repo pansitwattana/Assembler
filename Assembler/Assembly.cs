d9446f7a681adb48307225a291b92e504b0f393a
Pansit Wattanaprasosuk <jjsqling@gmail.com>
2016-10-20 23:02:26 +0700

    Add project files.

---
+++ Assembler/Assembly.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Assembler
+{
+    public class Assembly
+    {
+        public string Label { get; set; }
+        public string Instruction { get; set; }
+        public string Field0 { get; set; }
+        public string Field1 { get; set; }
+        public string Field2 { get; set; }
+        public string Comment { get; set; }
+        public string Type { get; set; }
+        public override string ToString()
+        {
+            return Label + "\t" + Instruction + "\t" + Field0 + "\t" + Field1 + "\t" + Field2 + "\t" + Comment;
+        }
+
+        public string ToMachine()
+        { 
+            switch (Type)
+            {
+                case "R":
+                    return ConvertTypeR();
+                case "I":
+                    return ConvertTypeI();
+                case "J":
+                    return ConvertTypeJ();
+                case "O":
+                    return ConvertTypeO();
+                default:
+                    return "Null";
+            }
+        }
+
+        private string ConvertTypeO()
+        {
+            return "";
+        }
+
+        private string ConvertTypeJ()
+        {
+            return "";
+        }
+
+        private string ConvertTypeI()
+        {
+            return "";
+        }
+
+        private string ConvertTypeR()
+        {
+            return "";
+        }
+    }
+}
---
+++ Assembler/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assembler
+{
+    class Program
+    {
+        static List<Assembly> assembies = new List<Assembly>();
+        static TextReader input = Console.In;
+        static void Main(string[] args)
+        {
+            Input(args);
+            Process();
+        }
+
+        private static void Input(string[] args)
+        {
+            ReadFromFile(args);
+        }
+
+        private static void ReadFromFile(string[] args)
+        {
+            if (args.Any())
+            {
+                var path = args[0];
+                if (File.Exists(path))
+                {
+                    input = File.OpenText(path);
+                }
+            }
+            
+            for (string line; (line = input.ReadLine()) != null;)
+            {
+                SplitText(line);
+            }
+        }
+
+        private static void SplitText(string inputText)
+        {
+
+            assembies.Add(new Assembly());
+        }
+
+        private static void Process()
+        {
+            foreach (Assembly assembly in assembies)
+            {
+                Console.WriteLine(assembly.ToMachine());
+            }
+        }
+    }
+}
