025b295c7dfc034c0bc62e6e8a278552429103fe
buckngaw <aon_natjaya@hotmail.com>
2016-10-20 23:57:38 +0700

    update assembly class (inst. machine)

--- Assembler/Assembly.cs
+++ Assembler/Assembly.cs
@@ -4,12 +4,22 @@ namespace Assembler
 {
     public class Assembly
     {
+
+        public Assembly(string label, string inst, string f0, string f1, string f2, string type)
+        {
+            Label = label;
+            Instruction = inst;
+            Field0 = f0;
+            Field1 = f1;
+            Field2 = f2;
+            Type = type;
+        }
+
         public string Label { get; set; }
         public string Instruction { get; set; }
         public string Field0 { get; set; }
         public string Field1 { get; set; }
         public string Field2 { get; set; }
-        public string Comment { get; set; }
         public string Type { get; set; }
         public override string ToString()
         {
@@ -17,7 +27,7 @@ namespace Assembler
         }
 
         public string ToMachine()
-        { 
+        {
             switch (Type)
             {
                 case "R":
@@ -40,19 +50,70 @@ namespace Assembler
 
         private string ConvertTypeJ()
         {
-            return "";
+            string ResultJ = "";
+            switch (Instruction)
+            {
+                case "jair":
+                    ResultJ += "101";
+                    break;
+            }
+            ResultJ += DecToBin(Field0);
+            return ResultJ;
         }
 
         private string ConvertTypeI()
         {
-            return "";
+            string ResultI = "";
+            switch (Instruction)
+            {
+                case "lW":
+                    ResultI += "010";
+                    break;
+                case "sw":
+                    ResultI += "011";
+                    break;
+                case "beq":
+                    ResultI += "100";
+                    break;
+
+            }
+            ResultI += DecToBin(Field0);
+            ResultI += ExtendZero(DecToBin(Field1));
+            return ResultI;
         }
 
         private string ConvertTypeR()
         {
-            return "";
+            string ResultR = "000";
+            switch (Instruction)
+            {
+                case "add":
+                    ResultR += "000";
+                    break;
+                case "nand":
+                    ResultR += "001";
+                    break;
+            }
+            ResultR += DecToBin(Field0);
+            ResultR += DecToBin(Field1);
+            ResultR += ExtendZero(DecToBin(Field2));
+            return ResultR;
         }
 
+        //Dec to Bin
+        private string DecToBin(string Binary)
+        {
+            //convert string to integer
+            //int m = Int32.Parse("abc");
+            int value = Int32.Parse(Binary);
+            string bin = Convert.ToString(value, 2);
+            return bin;
+        }
 
+        //zeroextend
+        private string ExtendZero(string bin)
+        {
+            return "";
+        }
     }
 }
